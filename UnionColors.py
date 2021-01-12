afb38d690e85c12082694d3842b7c08fb7a4f73c
tsanak <gtsanak1@gmail.com>
2021-01-12 21:50:02 +0200

    Add Union Colors Algorithm (group work)

---
+++ UnionColors.py
@@ -0,0 +1,44 @@
+import networkx as nx
+from collections import deque
+
+
+def run(G):
+    initial_nodes = ['RocknRolla', 'The League of Extraordinary Gentlemen', 'David Hemmings', 'Comedy']
+
+    # Enqueue all initial nodes
+    queue = deque([node for node in initial_nodes])
+    color = 1
+    initial_nodes.reverse()
+    for node in initial_nodes:
+        # Add color to the initial nodes
+        # the key is 'node' (id of node)
+        nx.set_node_attributes(G, {node: [color]}, 'colors')
+        color += 1
+
+    # Stop when you have K nodes (movie) that have all the colors
+    recommendations = []
+    k = 10
+    while len(queue) > 0 and k > 0:
+        # dequeue a node and visit it
+        current = queue.pop()
+        current_attr = G.nodes(data=True)[current]
+
+        if current_attr['node_type'] == 'movie' and \
+                len(current_attr['colors']) == len(initial_nodes) and \
+                current not in recommendations:
+            recommendations.append(current)
+            k -= 1
+        successors = dict(nx.bfs_successors(G, current, 1))
+        neighbors = successors[current]
+        for neighbor in neighbors:
+            if neighbor not in initial_nodes:
+                starting_node_color = G.nodes[current]['colors'][0]
+                neighbor_color_list = G.nodes[neighbor]['colors']
+                colors = [color for color in neighbor_color_list]
+                if starting_node_color not in neighbor_color_list:
+                    queue.append(neighbor)
+                    colors.append(starting_node_color)
+                    colors.reverse()
+                    nx.set_node_attributes(G, {neighbor: colors}, 'colors')
+
+    print(recommendations)
--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
+from typing import Optional, Any
+
 import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import UnionColors
 
 
-#movies.csv
+# movies.csv
 movies_data = pd.read_csv('movies.csv', delimiter=';')
 movies_data = movies_data[['movieID', 'title']]
 
@@ -24,13 +27,12 @@ actors_data.drop(['movieID'], axis=1, inplace=True)
 genre_data['edge'] = pd.Series(1, index=genre_data.index)
 genre_data.reset_index(drop=True)
 
-
 # Create a graph
 G = nx.Graph()
 # Add nodes
-G.add_nodes_from(actors_data.actorName, node_type='actor')
-G.add_nodes_from(movies_data.title, node_type='movie')
-G.add_nodes_from(genre_data.genre, node_type='genre')
+G.add_nodes_from(actors_data.actorName, node_type='actor', colors=[])
+G.add_nodes_from(movies_data.title, node_type='movie', colors=[])
+G.add_nodes_from(genre_data.genre, node_type='genre', colors=[])
 
 for gd in genre_data.values:
     G.add_edge(gd[1], gd[0], relation='movie_genre')
@@ -38,21 +40,24 @@ for gd in genre_data.values:
 for ac in actors_data.values:
     G.add_edge(ac[2], ac[0], relation='movie_actor')
 
+# Union Colors Algorithms
+UnionColors.run(G)
 
-color_map = []
-for node in G.nodes(data=True):
-    if 'node_type' in node[1]:
-        n = node[1].get('node_type')
-    else:
-        n = 'red'
-    if n == 'actor':
-        color_map.append('yellow')
-    elif n == 'movie':
-        color_map.append('blue')
-    elif n == 'genre':
-        color_map.append('green')
-    else:
-        color_map.append('red')
-
-nx.draw(G, node_color=color_map, with_labels=True)
-plt.show()
+#
+# color_map = []
+# for node in G.nodes(data=True):
+#     if 'node_type' in node[1]:
+#         n = node[1].get('node_type')
+#     else:
+#         n = 'red'
+#     if n == 'actor':
+#         color_map.append('yellow')
+#     elif n == 'movie':
+#         color_map.append('blue')
+#     elif n == 'genre':
+#         color_map.append('green')
+#     else:
+#         color_map.append('red')
+#
+# nx.draw(G, node_color=color_map, with_labels=True)
+# plt.show()
